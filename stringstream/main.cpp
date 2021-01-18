a1ca025e84866b0870a5ec709fdaec2e55fe747b
Brian Shewan <brian.shewan@nscc.ca>
2021-01-18 15:15:00 -0400

    added stringstream demo

--- stringstream/main.cpp
+++ stringstream/main.cpp
@@ -1,19 +1,33 @@
 #include <iostream>
+#include <sstream>
+#include <cstring>
 
 using namespace std;
 
 int main(int argc, char** argv) {
 
-    char command[256];
-    int start;
-    int end;
+    char input[256];
 
     cout << "Enter a command and two numbers: ";
-    cin >> command >> start >> end;
+    cin.getline(input, 256);
 
-    cout << "Command: " << command << endl;
-    cout << "Start: " << start << endl;
-    cout << "End: " << end << endl;
+    stringstream ss;
+    ss << input;
+
+    char command[256];
+    ss >> command;
+
+    if (strlen(command) > 1) {
+        cout << "You entered some text" << endl;
+    } else {
+        cout << "You entered a " << command << " command" << endl;
+
+        int start, end;
+        ss >> start;
+        ss >> end;
+
+        cout << "The command is from line " << start << " to " << end << endl;
+    }
 
     return 0;
 
