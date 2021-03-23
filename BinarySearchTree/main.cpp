573291849353ce7a01d99e612f4af2e78e6ce3e8
Brian Shewan <brian.shewan@nscc.ca>
2021-03-23 08:32:26 -0300

    added initial sorting demos

--- BinarySearchTree/main.cpp
+++ BinarySearchTree/main.cpp
@@ -11,18 +11,34 @@ typedef Node* NodePtr;
 class Node {
 public:
     int data;
-    Node* left;
-    Node* right;
+    NodePtr left;
+    NodePtr right;
 
     Node() : data(-1), left(nullptr), right(nullptr) {}
 };
 
 class BST {
 private:
-    Node* root;
+    NodePtr root;
 
 public:
     BST() : root(nullptr) {}
+
+    virtual ~BST() {
+        DeleteTree(root);
+    }
+
+    void DeleteTree(NodePtr node) {
+        if (node != nullptr) {
+            DeleteTree(node->left);
+            DeleteTree(node->right);
+
+            delete node;
+
+            node = nullptr;
+
+        }
+    }
 
     void Insert(int num) {
         Insert(num, root);
@@ -39,6 +55,70 @@ public:
         } else {
             cout << "Node value " << node->data << " already exists." << endl;
         }
+    }
+
+    void Remove(int num) {
+
+        NodePtr node = root;
+        NodePtr parent = nullptr;
+
+        while (node != nullptr) {
+            if (num < node->data) {
+                parent = node;
+                node = node->left;
+            } else if (num > node->data) {
+                parent = node;
+                node = node->right;
+            } else {
+                break;
+            }
+        }
+
+        if (node == nullptr) return;
+
+        // if a child has two children,
+        // use right-most node of left tree as successor
+        if (node->left != nullptr && node->right != nullptr) {
+
+            // start at left tree
+            NodePtr successor = node->left;
+
+            // keep going right as far as possible
+            parent = node;
+            while (successor->right != nullptr) {
+                parent = successor;
+                successor = successor->right;
+            }
+
+            // swap data with successor and successor is now the one to delete
+            node->data = successor->data;
+            node = successor;
+        }
+
+        // now the node to delete must have only one or no children
+
+        // assume there is left child
+        NodePtr subtree = node->left;
+
+        // if no left child, maybe a right child
+        if (subtree == nullptr) {
+            subtree = node->right;
+        }
+
+        // connect any children to new parents
+        if (parent == nullptr) {
+            // must be the root node
+            root = subtree;
+        } else if (parent->left == node) {
+            // deleting a left node of a parent
+            parent->left = subtree;
+        } else if (parent->right == node) {
+            // deleting a right node of parent
+            parent->right = subtree;
+        }
+
+        // finally!
+        delete node;
     }
 
     void PrintTree(ostream& output, NodePtr& node, int indent) {
@@ -70,6 +150,14 @@ int main() {
     bst.Insert(8);
 
     // test 1
+    cout << bst << endl;
+
+    // test 2
+    cout << "\n\nTest 2" << endl;
+    cout << "------" << endl;
+
+    bst.Remove(7);
+
     cout << bst << endl;
 
     return 0;
